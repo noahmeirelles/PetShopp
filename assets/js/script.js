877db236929b7b93894ad133e502780d69dbee0d
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-08-29 11:25:31 -0300

    feat: cadastro feito só falta exibir

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,5 +7,75 @@ class All {
         this.calendario = calendario;
     }
 }
-const teste = new All("noah", "branco", "branco", "ft", "224");
-console.log(teste)
+
+function cadastrar(){
+    let tutor = document.getElementById("tutor").value;
+    let name = document.getElementById("name").value;
+    let especie = document.getElementById("especie").value;
+    let foto = document.getElementById("foto").value;
+    let calendario = document.getElementById("calendario").value;
+    let show = new All(tutor, name, especie, foto, calendario);
+    console.log(show)
+lista.add(show);
+}
+//
+class List{
+    constructor(){
+        this.lista = [];
+    }
+    add(pet){
+        if (verify()) {
+            mensg("Preencha os campos", "errou");
+        } else {
+            this.lista.push(pet);
+            mensg("Cadastrado!", "permitido")
+            console.log(this.lista);
+        }
+    }
+    }
+    const lista = new List()
+
+    //
+
+    function mensg(msg, tipo){
+        let msgDiv = document.getElementById("msg");
+    msgDiv.innerHTML = "";
+
+    let msgParaTela = `
+        <p class='${tipo}'>${msg}</p>
+    `
+    msgDiv.innerHTML += msgParaTela;
+    
+
+    
+    setTimeout(function () {
+        msgDiv.innerHTML = "";
+    }, 3000);
+}
+    
+ //
+
+function verify() {
+    let tutor = document.getElementById("tutor").value;
+    let name = document.getElementById("name").value;
+    let especie = document.getElementById("especie").value;
+    let foto = document.getElementById("foto").value;
+    let calendario = document.getElementById("calendario").value;
+
+    
+    console.log({tutor});
+    console.log({ name });
+    console.log({ especie });
+    console.log({ foto });
+    console.log({ calendario });
+
+    if (tutor == "" || name == "" || especie == "" || foto == "" || calendario == "") {
+       
+        console.log("Ta vazio");
+        
+        return true;
+    } else {
+        console.log("Prft");
+        return false;
+    }
+}
