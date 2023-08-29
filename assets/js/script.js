f50aa332f6df57d0ae04c1e42815af29d6a04776
noahmeirelles <126571130+noahmeirelles@users.noreply.github.com>
2023-08-29 08:41:14 -0300

    feat: objeto feito e css quase completo com html

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+class All {
+    constructor(tutor, name, especie, foto, calendario){
+        this.tutor = tutor;
+        this.name = name;
+        this.especie = especie;
+        this.foto = foto;
+        this.calendario = calendario;
+    }
+}
+const teste = new All("noah", "branco", "branco", "ft", "224");
+console.log(teste)
